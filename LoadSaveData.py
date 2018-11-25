ae8a0caf136ab524aa8584e8f2f6c76a0472b90d
RachelNewhouse <ranewhouse43@gmail.com>
2018-11-25 21:51:35 +0000

    All necessary data can now be saved

--- LoadSaveData.py
+++ LoadSaveData.py
@@ -17,7 +17,13 @@ def collect_data_needing_saving(category_manager):
 			option_dict = {'name': option.name, 'responses_tuple': tuple_of_responses}
 			list_of_options.append(option_dict)
 		tuple_of_options = tuple(list_of_options)
-		data_in_category_to_save = {'name': category.name, 'options tuple': tuple_of_options}
+		list_of_start_times = []
+		for start_time in category.timer.start_times_list:
+			list_of_start_times.append({'year': start_time.year, 'month': start_time.month, 'day': start_time.day, 'hour': start_time.hour, 'minute': start_time.minute, 'second': start_time.second})
+		list_of_last_updated_times = []
+		for last_updated_time in category.timer.time_last_updated_list:
+			list_of_last_updated_times.append({'year': last_updated_time.year, 'month': last_updated_time.month, 'day': last_updated_time.day, 'hour': last_updated_time.hour, 'minute': last_updated_time.minute, 'second': last_updated_time.second})
+		data_in_category_to_save = {'name': category.name, 'options tuple': tuple_of_options, 'list of start times': list_of_start_times, 'list of last updated times': list_of_last_updated_times}
 		data_needing_saving.append(data_in_category_to_save)
 	return data_needing_saving
 
